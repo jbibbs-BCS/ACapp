f6d9956ce52a24c309a3dbc212dce8efa1649516
Josh Bibbs <joshuaebibbs@yahoo.com>
2026-06-28 13:31:56 -0500

    Initial Commit

---
+++ ArubaCentral/ArubaCentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArubaCentralApp.swift
+//  ArubaCentral
+//
+//  Created by Joshua Bibbs on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ArubaCentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
